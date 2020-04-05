80da22a797d72f71b0468c86e12879a4b029dfed
merothh <merothh@gmail.com>
2020-04-06 01:11:42 +0530

    config.h: switch to bottom bar

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = {
                                         "Meslo LG S:size=9",
                                         "FontAwesome 5 Free:style=Regular:size=8",
