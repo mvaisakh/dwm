e2635ee6651cdd44e16e6a8b09af6c3c638f0365
merothh <merothh@gmail.com>
2020-04-07 17:41:54 +0530

    config.h: bind prtsc key to scrshot
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- config.def.h
+++ config.def.h
@@ -69,9 +69,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *scrshotcmd[] = { "scrshot", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ 0, XK_Print, spawn, {.v = scrshotcmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
