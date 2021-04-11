9c24cc04015c4ad9f89f62ee8bc5d1f7db5deedd
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2021-04-11 22:49:58 +0530

    config.h: map keybindings for backlight brightness controls
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- config.def.h
+++ config.def.h
@@ -139,6 +139,8 @@ static Key keys[] = {
  	{ 0, XF86XK_AudioNext, spawn, SHCMD("playerctl next") },
  	{ 0, XF86XK_AudioPause, spawn, SHCMD("playerctl play-pause") },
 	{ 0, XF86XK_AudioPlay, spawn, SHCMD("playerctl play-pause") },
+	{ 0, XF86XK_MonBrightnessUp, spawn, SHCMD("xbacklight -inc 15") },
+ 	{ 0, XF86XK_MonBrightnessDown, spawn, SHCMD("xbacklight -dec 15") },
 };
 
 /* button definitions */
