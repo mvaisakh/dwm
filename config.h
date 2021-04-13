4e9263a89d885d519a732bb46a916fbce30316c7
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2021-04-14 00:30:59 +0530

    config.h: introduce col_black
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>

--- config.h
+++ config.h
@@ -20,6 +20,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_black[]       = "#000000";
 static const char col_accent[]      = "#0a88d6";
 static const unsigned int baralpha  = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
