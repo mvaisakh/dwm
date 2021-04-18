43c0399d943c0691866fded01615d79bfe74453a
bit6tream <bit6tream@cock.li>
2021-04-18 15:57:35 +0530

    dwm: Implement user changeable bar height
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- config.def.h
+++ config.def.h
@@ -8,6 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int barheight			= 22;		/* 0 means that dwm will calculate bar height, >= 1 means dwm will barheight as bar height */
 static const char *fonts[]          = { "monospace:size=7" };
 static const char dmenufont[]       = "monospace:size=7";
 static const char col_gray1[]       = "#222222";
