2e5f70f15ffa752b502e3bd97f07f8cf5ba01310
Mitch Weaver <mitch@wvr.sh>
2021-04-18 12:03:12 +0530

    dwm: Implement rounded corners
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -34,6 +34,7 @@ static const unsigned int alphas[][3]      = {
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
+static const int CORNER_RADIUS = 10; /* corner radius */
 
 /* tagging */
 static const char *tags[] = { "", "", "", "", "", "", "", "8", "9" };
